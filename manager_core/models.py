54cc13e4af787d4b2db7e460d5710aa1c92357eb
Yungon Park <hahafree12@gmail.com>
2016-07-16 21:51:45 +0900

    Add models for manage_core

--- manager_core/models.py
+++ manager_core/models.py
@@ -3,3 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    album_artist = models.CharField(max_length=200)
+    album_title = models.CharField(max_length=200)
+    album_cover_file = models.CharField(max_length=200)
+    album_url = models.CharField(max_length=200)
+
+class AlbumTracks(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    disk = models.IntegerField()
+    track_num = models.IntegerField()
+    track_title = models.CharField(max_length=200)
+    track_artist = models.CharField(max_length=200)
