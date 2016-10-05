18c2d8dd8ee0415a8e797950584ef03fed435945
Yungon Park <hahafree12@gmail.com>
2016-10-05 21:45:00 +0900

    Display album and track info on admin site (#14)

--- manager_core/models.py
+++ manager_core/models.py
@@ -9,9 +9,17 @@ class Album(models.Model):
     album_cover_file = models.CharField(max_length=200)
     album_url = models.CharField(max_length=200)
 
+    # Override __unicode__ method to display album artist and title.
+    def __unicode__(self):
+        return self.album_artist + " - " + self.album_title
+
 class AlbumTrack(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     disk = models.IntegerField()
     track_num = models.IntegerField()
     track_title = models.CharField(max_length=200)
     track_artist = models.CharField(max_length=200)
+
+    # Override __unicode__ method to display song artist and title.
+    def __unicode__(self):
+        return self.track_artist + " - " + self.track_title
