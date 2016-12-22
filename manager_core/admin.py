1af5d936f8dfdaa28cde5aefb678f7eb23ff0023
Yungon Park <hahafree12@gmail.com>
2016-12-22 10:41:52 +0900

    Modify admin site to create/update/delete album info manually. (#14)

--- manager_core/admin.py
+++ manager_core/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 
 from .models import Album, AlbumTrack
 
+
+# Track Inline.
+class AlbumTrackInline(admin.TabularInline):
+    model = AlbumTrack
+    extra = 1
+
+
+# Album Admin
+class AlbumAdmin(admin.ModelAdmin):
+    inlines = [AlbumTrackInline]
+    list_display = ("album_artist", "album_title")
+
+
+# Album Track Admin
+class AlbumTrackAdmin(admin.ModelAdmin):
+    list_display = ("album", "disk", "track_num", "track_title", "track_artist")
+
 # Register your models here.
-admin.site.register(Album)
-admin.site.register(AlbumTrack)
+admin.site.register(Album, AlbumAdmin)
+admin.site.register(AlbumTrack, AlbumTrackAdmin)
