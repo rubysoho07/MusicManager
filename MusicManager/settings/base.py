0a4b2a5c8e26c2f2efbdbea06c7e542e40947fc5
Yungon Park <hahafree12@gmail.com>
2017-02-04 17:38:26 +0900

    Add config of 'mm_user' app. (#15)

--- MusicManager/settings/base.py
+++ MusicManager/settings/base.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'manager_core.apps.ManagerCoreConfig',
+    'mm_user.apps.MmUserConfig',
     'crispy_forms',
 ]
 
