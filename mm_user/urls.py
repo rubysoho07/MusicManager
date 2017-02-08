6abbab7a72e980c683db2f4eed0ece6a2fc3d4e9
Yungon Park <hahafree12@gmail.com>
2017-02-08 21:44:51 +0900

    Add modifying user information. (#15)

--- mm_user/urls.py
+++ mm_user/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     url(r'^create/', UserCreateView.as_view(), name='create'),
     url(r'^create_done/', UserCreateDoneTV.as_view(), name='create_done'),
     url(r'^profile/(?P<pk>\d+)$', UserDetailView.as_view(), name='user_profile'),
-    url(r'^main/$', UserMainView.as_view(), name='main')
+    url(r'^main/$', UserMainView.as_view(), name='main'),
+    url(r'^update/$', UserUpdateView.as_view(), name='update'),
 ]
--- mm_user/views.py
+++ mm_user/views.py
@@ -1,25 +1,29 @@
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.core.urlresolvers import reverse_lazy
 
-from mm_user.forms import UserCreationForm
+from mm_user.forms import UserCreationForm, UserChangeForm
 from mm_user.models import MmUser
 
 
+# User creation form.
 class UserCreateView(CreateView):
     template_name = 'users/register.html'
     form_class = UserCreationForm
     success_url = reverse_lazy('user:create_done')
 
 
+# After creating a user, redirect to 'register done' page.
 class UserCreateDoneTV(TemplateView):
     template_name = 'users/register_done.html'
 
 
+# View to see user's profile
 class UserDetailView(DetailView):
     model = MmUser
     template_name = 'users/user_main.html'
@@ -28,13 +32,25 @@ class UserDetailView(DetailView):
         return super(UserDetailView, self).get_context_data(**kwargs)
 
 
+# View to see current user's profile
 class UserMainView(LoginRequiredMixin, UserDetailView):
     model = MmUser
     template_name = 'users/user_main.html'
 
+    # Get current user.
     def get_object(self):
         return self.request.user
 
     def get_context_data(self, **kwargs):
         return super(UserMainView, self).get_context_data(**kwargs)
 
+
+# Modify user's profile.
+class UserUpdateView(LoginRequiredMixin, UpdateView):
+    form_class = UserChangeForm
+    template_name = 'users/modify.html'
+    success_url = reverse_lazy('user:main')
+
+    # Get current user.
+    def get_object(self):
+        return self.request.user
