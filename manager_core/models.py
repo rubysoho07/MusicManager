e6040c9323b016b4925acf02a34734e517800f3d
Yungon Park <hahafree12@gmail.com>
2017-02-27 10:02:20 +0900

    Add features for adding/deleting comments on an album.

--- manager_core/models.py
+++ manager_core/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from django.utils.encoding import python_2_unicode_compatible
 
 from django.db import models
+from django.conf import settings
 
 
 # Create your models here.
@@ -29,3 +30,14 @@ class AlbumTrack(models.Model):
     # Override __unicode__ method to display song artist and title.
     def __str__(self):
         return self.track_artist + " - " + self.track_title
+
+
+@python_2_unicode_compatible
+class AlbumComment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=500)
+    add_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user + "/" + self.album + "/" + self.comment
