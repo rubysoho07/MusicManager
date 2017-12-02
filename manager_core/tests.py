b7ea7d28c46f0fb0768adb4ddd74d5177e8ba2d4
Yungon Park <hahafree12@gmail.com>
2017-12-02 10:34:46 +0900

    (#33) Try to use Travis CI & Coveralls

--- manager_core/tests.py
+++ manager_core/tests.py
@@ -14,18 +14,18 @@ class MusicParserTestCase(TestCase):
     
     Example 1, 2 are for Korean music sites. (Bugs, Melon, Naver)
     Example 1: Crying Nut/No Brain - 96
-    Example 2: Various Artists - Club Heavy 20th
+    Example 2: Pink Floyd - The Wall
     
     Example 3, 4 are for AllMusic.
     Example 3: The Smashing Pumpkins - Mellon Collie and the Infinite Sadness
     Example 4: Various Artists - Judgment Night OST
     """
     bugs_example_1 = "http://music.bugs.co.kr/album/450734"
-    bugs_example_2 = "http://music.bugs.co.kr/album/20032982"
+    bugs_example_2 = "https://music.bugs.co.kr/album/8000460"
     melon_example_1 = "http://www.melon.com/album/detail.htm?albumId=2281828"
-    melon_example_2 = "http://www.melon.com/album/detail.htm?albumId=2681984"
+    melon_example_2 = "http://www.melon.com/album/detail.htm?albumId=2661487"
     naver_music_example_1 = "http://music.naver.com/album/index.nhn?albumId=451880"
-    naver_music_example_2 = "http://music.naver.com/album/index.nhn?albumId=634351"
+    naver_music_example_2 = "http://music.naver.com/album/index.nhn?albumId=12873"
     all_music_example_3 = "http://www.allmusic.com/album/mellon-collie-and-the-infinite-sadness-mw0000645152"
     all_music_example_4 = "http://www.allmusic.com/album/judgment-night-mw0000101514"
 
@@ -63,8 +63,8 @@ class MusicParserTestCase(TestCase):
         """Check album cover patterns from music information sites."""
         naver_pattern = "http://musicmeta.phinf.naver.net/album/000/645/645112.jpg?type=r204Fll&v=20160623150347"
         melon_pattern = "http://cdnimg.melon.co.kr/cm/album/images/006/23/653/623653.jpg"
-        bugs_pattern = "http://image.bugsm.co.kr/album/images/200/5712/571231.jpg"
-        all_music_pattern = "http://cps-static.rovicorp.com/3/JPG_500/MI0002/416/MI0002416076.jpg?partner=allrovi.com"
+        bugs_pattern = "https://image.bugsm.co.kr/album/images/200/5712/571231.jpg"
+        all_music_pattern = "https://cps-static.rovicorp.com/3/JPG_500/MI0002/416/MI0002416076.jpg?partner=allrovi.com"
 
         naver_result = MusicParser.check_album_cover_pattern(naver_pattern)
         self.assertEqual(naver_result, True)
@@ -90,8 +90,8 @@ class MusicParserTestCase(TestCase):
 
         result2 = self.naver_music_parser.parse_album(self.naver_music_example_2)
         result2_json = json.loads(result2)
-        self.assertEqual(result2_json['artist'], "Various Artists")
-        self.assertIn("CLUB HEAVY 20th", result2_json['album_title'])
+        self.assertEqual(result2_json['artist'], "Pink Floyd")
+        self.assertIn("The Wall", result2_json['album_title'])
 
     def test_check_melon(self):
         """Check parsing from Melon."""
@@ -102,11 +102,11 @@ class MusicParserTestCase(TestCase):
 
         result2 = self.melon_parser.parse_album(self.melon_example_2)
         result2_json = json.loads(result2)
-        self.assertEqual(result2_json['artist'], "Various Artists")
-        self.assertIn("Club Heavy 20th", result2_json['album_title'])
+        self.assertEqual(result2_json['artist'], "Pink Floyd")
+        self.assertIn("The Wall", result2_json['album_title'])
 
     def test_check_bugs(self):
-        """Check parsing from Naver Music."""
+        """Check parsing from Bugs."""
         result1 = self.bugs_parser.parse_album(self.bugs_example_1)
         result1_json = json.loads(result1)
         self.assertEqual(result1_json['artist'], "크라잉넛(Crying Nut), 노브레인(No Brain)")
@@ -114,8 +114,8 @@ class MusicParserTestCase(TestCase):
 
         result2 = self.bugs_parser.parse_album(self.bugs_example_2)
         result2_json = json.loads(result2)
-        self.assertEqual(result2_json['artist'], "Various Artists")
-        self.assertIn("Club Heavy 20th", result2_json['album_title'])
+        self.assertEqual(result2_json['artist'], "Pink Floyd(핑크 플로이드)")
+        self.assertIn("The Wall", result2_json['album_title'])
 
     def test_check_allmusic(self):
         """Check parsing from AllMusic."""
