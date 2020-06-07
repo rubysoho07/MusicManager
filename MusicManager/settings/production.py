eb66bcea5ce1442951df13f07868d3b404385097
Yungon Park <hahafree12@gmail.com>
2020-06-07 10:23:53 +0900

    Modify not to use AWS access key & secret access key for production environment.

--- MusicManager/settings/production.py
+++ MusicManager/settings/production.py
@@ -23,8 +23,10 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+# Access key & secret access key are not needed for zappa + django-storage
+# Reference: https://github.com/jschneier/django-storages/issues/606
+# AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+# AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_REGION_NAME = os.environ.get("AWS_S3_REGION_NAME")
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_FILE_OVERWRITE = True
